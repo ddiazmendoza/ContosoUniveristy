561b0fd11178988f8d590617d407139f88394bf0
Diego D <56505625+ddiazmendoza@users.noreply.github.com>
2021-06-07 12:24:07 -0500

    students CRUD done

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -18,14 +18,14 @@ namespace ContosoUniversity.Data
 
             var students = new Student[]
             {
-            new Student{Nombre="Carson",Apellido="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
-            new Student{Nombre="Meredith",Apellido="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{Nombre="Arturo",Apellido="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new Student{Nombre="Gytis",Apellido="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{Nombre="Yan",Apellido="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
-            new Student{Nombre="Peggy",Apellido="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
-            new Student{Nombre="Laura",Apellido="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
-            new Student{Nombre="Nino",Apellido="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+            new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
             };
             foreach (Student s in students)
             {
